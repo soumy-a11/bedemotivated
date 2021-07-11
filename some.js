31a43f5ebeab61e751768a8877d12d529f1bab6a
soumy johar <66939818+soumy-a11@users.noreply.github.com>
2021-07-11 18:04:29 +0530

    Add files via upload

---
+++ some.js
@@ -0,0 +1,13 @@
+var btn = 't'
+
+function c()
+{
+    var btn1 = document.getElementById("Button");
+    if(btn== 't'){
+        btn1.value = 'you are noob!'
+        btn = 'f';
+    }else{
+        btn1.value = 'you are very noob', 'nooooooooooob';
+        btn = 't';
+    }
+}
